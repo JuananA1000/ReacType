bd8552d355a22f9e8c2cc0e57bcb8fec9915d8d5
Juanan <juanantonioamillopez@gmail.com>
2024-04-10 11:48:19 +0200

    Añadir contador de palabras

---
+++ src/components/Contador/Contador.jsx
@@ -0,0 +1,9 @@
+import './Contador.css'
+
+export default function Contador() {
+  return (
+    <div>
+      <p id='contador'>0/0</p>
+    </div>
+  );
+}
--- src/components/Frase/Frase.jsx
+++ src/components/Frase/Frase.jsx
@@ -1,9 +1,15 @@
-import './Frase.css'
+import Contador from '../Contador/Contador';
+
+import './Frase.css';
 
 export default function Frase() {
   return (
     <div>
-      <p id='frase'>Frase</p>
+      <Contador />
+      <p id='frase'>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut incidunt dolores porro cum, debitis id aliquam ad
+        vitae consequatur adipisci labore sed deserunt omnis cupiditate in tenetur voluptates vel. Ducimus.
+      </p>
     </div>
   );
 }
