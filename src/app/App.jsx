1239a36c6a278dd0644b33032932fdb57b6f1b6e
Juanan <juanantonioamillopez@gmail.com>
2024-04-11 10:51:48 +0200

    Ordenar las palabras aleatoriamente

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,17 +1,24 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import Logo from '../components/Logo/Logo';
 import './App.css';
 
-function App() {
-  const cloud = `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id ipsum quae odit in magni totam consectetur
-  vitae, enim harum rerum maxime fugiat inventore nostrum corrupti labore explicabo similique quam et.`;
+const getCloud = () =>
+  `Lorem ipsum dolor sit amet consectetur adipisicing elit Id ipsum quae odit in magni totam consectetur vitae enim
+  harum rerum maxime fugiat inventore nostrum corrupti labore explicabo similique quam et`
+    .split(' ')
+    .sort(() => (Math.random() > 0.5 ? 1 : -1));
 
+function App() {
   const [userInput, setUserInput] = useState('');
+  const cloud = useRef(getCloud());
+
+  console.log('Contenido de getCloud: ', cloud.current);
+
   return (
     <div>
       <Logo />
 
-      <p>{userInput}</p>
+      <p>{cloud.current.join(' ')}</p>
       <input type='text' value={userInput} onChange={(e) => setUserInput(e.target.value)} />
     </div>
   );
