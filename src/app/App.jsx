b7b440580965c8b3b01adfa26685810a44452c49
Juanan <juanantonioamillopez@gmail.com>
2024-04-11 09:39:52 +0200

    Agregar palabras del test

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,6 +3,9 @@ import Logo from '../components/Logo/Logo';
 import './App.css';
 
 function App() {
+  const cloud = `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id ipsum quae odit in magni totam consectetur
+  vitae, enim harum rerum maxime fugiat inventore nostrum corrupti labore explicabo similique quam et.`;
+
   const [userInput, setUserInput] = useState('');
   return (
     <div>
