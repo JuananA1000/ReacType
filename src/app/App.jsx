fa4383c2394d5c1f44b2b6a9e81ea585d4248c92
Juanan <juanantonioamillopez@gmail.com>
2024-04-11 09:39:18 +0200

    Agregar input funcional

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,11 +1,15 @@
+import { useState } from 'react';
 import Logo from '../components/Logo/Logo';
-
 import './App.css';
 
 function App() {
+  const [userInput, setUserInput] = useState('');
   return (
     <div>
       <Logo />
+
+      <p>{userInput}</p>
+      <input type='text' value={userInput} onChange={(e) => setUserInput(e.target.value)} />
     </div>
   );
 }
