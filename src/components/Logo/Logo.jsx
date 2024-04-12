4b722af220407c81c4303d32b08593b6430c5baf
Juanan <juanantonioamillopez@gmail.com>
2024-04-12 13:40:28 +0200

    Insertar logo y reorganizar archivos

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import logo from '../../assets/images/logo.svg';
+
+import './Logo.css';
 
 export default function Logo() {
   return (
-    <div>
+    <div className='logo'>
+      <img src={logo} alt='logo' width={50} />
       <h1>ReacType</h1>
     </div>
   );
